cc593c15974a4cd2fa1966d223d83db5ea96b918
irenehernandezvelarde <ihernandezvelarde.cf@iesesteveterradas.cat>
2023-01-17 16:54:22 +0100

    //Añadir Boton para capturar imagen y que salga en la carpeta Files

--- app/src/main/java/com/example/camerax/CameraActivity.java
+++ app/src/main/java/com/example/camerax/CameraActivity.java
@@ -1,15 +1,21 @@
 package com.example.camerax;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Size;
 import android.view.OrientationEventListener;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -19,6 +25,7 @@ import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
 import java.util.concurrent.ExecutionException;
 
 public class CameraActivity extends AppCompatActivity {
@@ -33,7 +40,9 @@ public class CameraActivity extends AppCompatActivity {
         previewView = findViewById(R.id.previewView);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         textView = findViewById(R.id.orientation);
+
         cameraProviderFuture.addListener(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void run() {
                 try {
@@ -46,7 +55,7 @@ public class CameraActivity extends AppCompatActivity {
         }, ContextCompat.getMainExecutor(this));
     }
 
-
+    @RequiresApi(api = Build.VERSION_CODES.R)
     private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
@@ -60,7 +69,6 @@ public class CameraActivity extends AppCompatActivity {
         OrientationEventListener orientationEventListener = new OrientationEventListener(this) {
             @Override
             public void onOrientationChanged(int orientation) {
-                //textView.setText(Integer.toString(orientation)); NUMBER!!!
             }
         };
         orientationEventListener.enable();
@@ -68,7 +76,32 @@ public class CameraActivity extends AppCompatActivity {
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,
-                imageAnalysis, preview);
+
+        Button capturarImagen = findViewById(R.id.foto);
+
+        ImageCapture imageCapture =
+                new ImageCapture.Builder()
+                        .setTargetRotation(this.getDisplay().getRotation())
+                        .build();
+        capturarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(new File(getFilesDir()+File.separator +"Foto.jpg")).build();
+                imageCapture.takePicture(outputFileOptions,ContextCompat.getMainExecutor(CameraActivity.this),
+                        new ImageCapture.OnImageSavedCallback() {
+                            @Override
+                            public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                            }
+                            @Override
+                            public void onError(@NonNull ImageCaptureException exception) {
+
+                            }
+                        }
+                );
+            }
+        });
+
+        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageCapture,imageAnalysis, preview);
     }
+
 }
